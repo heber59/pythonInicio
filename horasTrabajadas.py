10f8860de94b1f70840e97df7a7a4c593c30f4b5
heber59 <hesan0308@gmail.com>
2023-12-04 12:11:12 -0500

    solucion horasTrabajadas, ejercicio 3 y final

--- horasTrabajadas.py
+++ horasTrabajadas.py
@@ -2,53 +2,46 @@
 #sueldo 40 horas, sueldo normal
 #mayor que 40 y = a 48 es un 20% mas
 #49 o mas valen 40% mas
-def pagos (normal,extra,extraDobles):
-    print("hola")
-
-def extra():
-    print("extra")
-
-def extraDobles():
-    print("extradobles")
-
-def normal():
-    print("normal")
-
-
+def pagos (pagoTotal):
+    print("Digite cuantas horas trabajo",datos[0])
+    horasTrabajadas=int(input())
+    print("Digite cuanto se le paga la hora")
+    pagoHora=int(input())
+    print(pagoHora, pagoHora+1)
+    if horasTrabajadas > 48 :
+        for _ in range (horasTrabajadas-48) :
+            pagoTotal = pagoHora + pagoTotal + (pagoHora*0.4)
+            horasTrabajadas -= 1
+    if horasTrabajadas > 40 : 
+        for _ in range (horasTrabajadas-40) :
+            pagoTotal = pagoTotal + pagoHora+(pagoHora*0.2)
+            horasTrabajadas -= 1
+    pagoTotal = pagoTotal + (horasTrabajadas*pagoHora)  
+    pagoTotalizado = pagoTotal
+    return pagoTotal                 
 def registro():
-    datos = []
     datos.append(input(" ingrese el nombre de el empleado\n"))
     datos.append(input(" ingrese el apellido de el empleado\n"))
     datos.append(input(" ingrese el telefono de el empleado\n"))
     datos.append(input(" ingrese la cedula de el empleado\n"))
-    print(datos)
-    datosConfirmar = int
-    while datosConfirmar != "n" :
-        datosConfirmar=int(input(" Desea modificar datos s/n "))
-        if datosConfirmar == "s":
-            print("ingrese el dato que va a modificar")
-            cambio= input()
-            if cambio in datos:
-                print("cual es el nuevo dato que desea")
-                nuevoDato=input()
-                
-
-        
-
-
-
-
+    return  datos
 
 def imprimir():
-    print("imprime")
+    print("|  ",datos)
+    print("|")
+    print("|   genero factura por:",generarpagos)
+    print("|")
+  
 
 
+pago = 0.0
+datos = []
 repBucle = int
 while  repBucle  != 4 :
     repBucle=int(input("1.registar empleado\n2.registrar horas trabajadas y pago\n3.imprimir datos\n4.salir\n"))
     if repBucle == 1 :
         registro()
     elif repBucle == 2:
-        pagos(normal(),extra(),extraDobles())
+        generarpagos= pagos (pago)
     elif repBucle == 3:
         imprimir()
